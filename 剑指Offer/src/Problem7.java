134ea4a069301c64ba29445b8c1a5e86fd528fcc
Yisaer <2695690803@qq.com>
2017-04-08 15:56:06 +0800

    Fib

---
+++ 剑指Offer/src/Problem7.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Yisa on 2017/4/8.
+ */
+public class Problem7  {
+    boolean isPre = false;
+    int[] Fib = new int[40];
+    void init(){
+        Fib[1] = 1;
+        Fib[2] = 1;
+        for(int i = 3;i<=39;i++){
+            Fib[i] = Fib[i-1]+Fib[i-2];
+        }
+        isPre = true;
+    }
+    public int Fibonacci(int n) {
+        if(isPre){
+            return Fib[n];
+        }
+        else{
+            init();
+            return Fib[n];
+        }
+    }
+
+
+}
