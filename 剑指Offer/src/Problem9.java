dd550eda205b36e857e6cf3105db8051d22b8f4d
Yisaer <2695690803@qq.com>
2017-04-08 16:12:11 +0800

    JUMP2

--- 剑指Offer/src/Problem9.java
+++ 剑指Offer/src/Problem9.java
@@ -2,4 +2,22 @@
  * Created by Yisa on 2017/4/8.
  */
 public class Problem9 {
+    private boolean I = false;
+    private int[] F = new int[999];
+    private  void init(){
+        I = true;
+        F[1] = 1;
+        for(int i  = 2;i<999;i++){
+            F[i] = 1;
+            for(int  j =1;j<i;j++){
+                F[i]+=F[j];
+            }
+        }
+    }
+    public int JumpFloorII(int target) {
+        if(!I){
+            init();
+        }
+        return F[target];
+    }
 }
